358099cf75b622cb18b04c9be9cdaa2af633535d
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-19 23:32:23 +0000

    new changes

--- app.rb
+++ app.rb
@@ -2,16 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:layout)
+  erb(:dice_form)
 end
 
-get("/process_roll/?dice=:dice_num&sides=:side_num") do
+get("/process_roll") do
   @rolls = []
   # use the fetch method to get what the user inputs 
   # need to convert it to integer because it comes as a string
-  @dice_num = params.fetch("dice_num").to_i
-  @side_num = params.fetch("side_num").to_i
-  
+  @dice_num = params.fetch("dice").to_i
+  @side_num = params.fetch("sides").to_i
+
   @dice_num.times do
     die = rand(1..@side_num)
 
